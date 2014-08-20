5a20ede50720d5e69785cc2252b061739641212c
WaiYu <waiyuhsieh@gmail.com>
2014-08-19 23:40:38 -0700

    Update ChocolateFeast.py

--- ChocolateFeast.py
+++ ChocolateFeast.py
@@ -3,7 +3,7 @@ for i in range (0,T):
     money,price,exchange = [int(x) for x in raw_input().split(' ')]
     
     total_choco = money/price
-    wrapper = answer
+    wrapper = total_choco
     while wrapper >= exchange:
         total_choco += wrapper / exchange
         wrapper = wrapper / exchange + wrapper % exchange
